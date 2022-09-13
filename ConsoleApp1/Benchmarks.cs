9e97b03f0dc4f6a59814f8867e00c90e8767adf7
trickshot2012 <trickshot2012@yahoo.de>
2022-09-13 02:03:30 +0200

    Add project files.

---
+++ ConsoleApp1/Benchmarks.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPerformance
+{
+    [MemoryDiagnoser(false)]
+  
+    public class Benchmarks
+    {
+        [Params(100)]
+        public int Size { get; set; }
+
+        private IEnumerable<int> _items;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _items = Enumerable.Range(1,Size).ToArray();
+        }
+
+        [Benchmark]
+        public int Max()=> _items.Max();
+
+        [Benchmark]
+        public int Min()=> _items.Min();
+
+        [Benchmark]
+        public double Average()=> _items.Average();
+
+        [Benchmark]
+        public int Sum() => _items.Sum();   
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿//var item = Enumerable.Range(1, 100);
+
+
+//var max=item.Max();
+//var min = item.Min();
+//var average=item.Average();
+//var sum=item.Sum();
+using BenchmarkDotNet.Running;
+using LinqPerformance;
+
+BenchmarkRunner.Run<Benchmarks>();
